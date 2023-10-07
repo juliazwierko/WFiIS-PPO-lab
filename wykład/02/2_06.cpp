a8d3b839c8b5b357ee3ca25de8c42cf97d1d68c6
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-10-07 13:46:56 +0200

    Create 2_06.cpp

---
+++ wykład/02/2_06.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	int a = 10, b = 5, c = 3;
+	
+	cout << "a + b = " << a + b << endl;
+	cout << "a - b = " << a - b << endl;
+	cout << "a * b = " << a * b << endl;
+	cout << "a / b = " << a / b << endl;
+	cout << "a % b = " << a % b << endl;
+	cout << "a % c = " << a % c << endl << endl;
+
+	int i = 0;
+	cout << "++i = " << ++i << endl;
+	i = 0;
+	cout << "i++ = " << i++ << endl;
+	i = 0;
+	cout << "--i = " << --i << endl;
+	i = 0;
+	cout << "i-- = " << i-- << endl;
+}
+
+// a + b = 15
+// a - b = 5
+// a * b = 50
+// a / b = 2
+// a % b = 0
+// a % c = 1
+
+// ++i = 1
+// i++ = 0
+// --i = -1
+// i-- = 0#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	int a = 10, b = 5, c = 3;
+	
+	cout << "a + b = " << a + b << endl;
+	cout << "a - b = " << a - b << endl;
+	cout << "a * b = " << a * b << endl;
+	cout << "a / b = " << a / b << endl;
+	cout << "a % b = " << a % b << endl;
+	cout << "a % c = " << a % c << endl << endl;
+
+	int i = 0;
+	cout << "++i = " << ++i << endl;
+	i = 0;
+	cout << "i++ = " << i++ << endl;
+	i = 0;
+	cout << "--i = " << --i << endl;
+	i = 0;
+	cout << "i-- = " << i-- << endl;
+}
+
+// a + b = 15
+// a - b = 5
+// a * b = 50
+// a / b = 2
+// a % b = 0
+// a % c = 1
+
+// ++i = 1
+// i++ = 0
+// --i = -1
+// i-- = 0
