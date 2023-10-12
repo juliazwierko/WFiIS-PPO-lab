266fcf87dcd0dfbe0694decbe1488e424a2358c3
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-10-12 19:47:13 +0200

    Create Main.cpp

---
+++ 01/Main.cpp
@@ -0,0 +1,47 @@
+// To jest przykład instrukcji do ćwiczeń - kompletna instrukcja 
+// zawsze będzie zawarta w tym miejscu, tj. w pliku Main.cpp
+// Przykład polecenia 
+// Prosze dopisac kod, dodac nowe pliki, tak aby program wykonywal się
+// a wynik jego dzialania byl taki sam jak podany na końcu tego pliku
+//
+// Pliku Main.cpp, nie wolno modyfikowac.
+//
+// Ostateczny program powinien byc przyjazny dla programisty
+// (miec czytelny i dobrze napisany kod)
+// - co to znaczy? 
+//
+// Przy wykonaniu zadania nie wolno korzystac z internetu, notatek,
+// ani żadnych innych materialow (w tym własnych wczesniej
+// przygotowanych plikow oprócz makefile, chyba że polecenie stanowi inaczej)
+//
+//
+// Kody źródłowe muszą znajdować się w katalogu do którego nikt oprócz
+// właściciela nie ma praw dostępu.
+// Rozwiazanie (czyli dodane pliki i Main.cpp należy wgrać do UPEL 
+// jako archiwum tar.gz o strukturze takiej jak w zadaniu na UPeL.
+// UWAGA:
+// * archiwum powinno zawierać katalog z zadaniem lab01/
+// * katalog z zadaniem powinien zawierać podkatalog include/
+//   w którym muszą znajdować się wszystkie niezbędne pliki nagłówkowe
+// * katalog z zadaniem powinien zawierać podkatalog src/
+//   w którym muszą znajdować się wszystkie niezbędne pliki z kodem źródłowym
+// * katalog z zadaniem powinien zawierać podkatalog build/ 
+//   w którym powinien znajdować się tylko i wyłącznie plik Makefile
+// * tworzenie archiwum: tar -czvf nazwa.tar.gz zad/
+//   np. tar -czvf lab01.tar.gz lab01/
+// * rozpakowanie archiwum: tar -xvf nazwa.tar
+
+
+#include "MyLibrary.h"
+
+int main() {
+  PrintName("Gall Anonim");
+  const char* path = PROJECTPATH;
+  PrintInfo(PROJECTPATH);
+  return 0;
+}
+/** Wynik dzialania programu:
+[INFO]:: Name: Gall Anonim
+[INFO]:: /home/ganonim/workspace/ppo/2023_2024/lab01/ex
+*/
+// Oczywiście, ścieżka do projektu będzie się różnić.
