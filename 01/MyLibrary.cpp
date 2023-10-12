41997f4185549fe7a458d4e3f64682e57c98f65b
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-10-12 19:48:19 +0200

    Update MyLibrary.cpp

--- 01/MyLibrary.cpp
+++ 01/MyLibrary.cpp
@@ -3,7 +3,8 @@
 /**
  * @brief ta funkcja sluzy dla wypisywania name'a uzytkownika
  * @param funkcja przyjmuje zmienna typu const char *, czyli nasz name
- * @return zwaraca wartosc typu void, czyli nic. Mamy tylko std::cout (wypisywanie tego,  co podalismy jako argument)
+ * @return zwaraca wartosc typu void, czyli nic. Mamy tylko std::cout (wypisywanie tego, 
+ * co podalismy jako argument)
 */
 void PrintName(const char *a){
     std::cout << "[INFO]:: Name: " << a << std::endl;
@@ -12,7 +13,8 @@ void PrintName(const char *a){
 /**
  * @brief ta funkcja sluzy dla wypisywania sciezki do file'a
  * @param funkcja przyjmuje zmienna typu const char *, czyli sciezke
- * @return zwaraca void, czyli nic. Mamy tylko std::cout (wypisywanie tego,  co podalismy jako argument)
+ * @return zwaraca void, czyli nic. Mamy tylko std::cout (wypisywanie tego,
+ * co podalismy jako argument)
 */
 void PrintInfo(const char *a){
     std::cout << "[INFO]:: " << a << std::endl;
