5925ac3052b314df7ef03e9068ab1847433b1ac2
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-10-12 19:47:21 +0200

    Add files via upload

---
+++ 01/MyLibrary.h
@@ -0,0 +1,9 @@
+#pragma once 
+
+#include <iostream>
+//to jest sciezka do plika
+#define PROJECTPATH  "/home/ganonim/workspace/ppo/2023_2024/lab01/ex"
+//#define PROJECTPATH __FILE__
+
+void PrintName(const char *a);
+void PrintInfo(const char *a);
