ad6d37b8b599b28a40309505ae55614523b484d3
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-10-24 15:42:13 +0200

    Add files via upload

---
+++ przygotowanie do zajęć laboratoryjnych/03 (25.10)/GSeries.h
@@ -0,0 +1,11 @@
+#pragma once 
+
+#include <iostream>
+
+#define rozmiarCiagu int
+#define ciag float*
+
+float* InicjalizujCiagGeometryczny(const int size, const float iloczyn);
+
+void UsunCiagi();
+
