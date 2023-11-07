7999a76cc78b3181f8b89d6029d685878af1c6e8
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-07 16:01:18 +0100

    Update 02.cpp

--- wykład/04/02.cpp
+++ wykład/04/02.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+class Punkt {
+
+public:
+    void SetX(double x){
+        _x = x;
+    }
+    void SetY(double y){
+        _y = y;
+    }
+    double GetX() const{
+        return _x;
+    }
+    double GetY() const{
+        return _y;
+    }
+
+    private:
+        double _x;
+        double _y;
+};
+
+int main(int argc, char **argv){
+    Punkt aPunkt;
+
+    aPunkt.SetX(10.0);
+    aPunkt.SetY(10.0);
+
+    cout << "a._x, a._y = " << aPunkt.GetX() << ", " << aPunkt.GetX() << endl;
+	// cout << "a._x, a._y = " << aPunkt._x << ", " << aPunkt._y << endl;  //BLAD
+    return 0;
+}
+
+// a._x, a._y = 10, 10
