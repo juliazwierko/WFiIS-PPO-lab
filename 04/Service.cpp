2ec604cacdcb60d038865aa0b20f4a6656add4ab
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-13 11:06:48 +0100

    Update Service.cpp

--- 04/Service.cpp
+++ 04/Service.cpp
@@ -1,1 +1,28 @@
 #include "Service.h"
+#include <iostream>
+
+using namespace std;
+
+void Service::TestCopy(Car car, int distance){
+    cout << "#############################################################\n";
+    cout << "### Test serwisowy na egzemplarzu fabrycznym o identycznych parametrach ###\n\n";
+    TestCar(&car, distance);
+}
+
+void Service::Test(Car * pcar, int distance){
+    cout << "#############################################################\n";
+    cout << "### Test serwisowy ###\n\n";
+    TestCar(pcar, distance);
+}
+
+void TestCar(Car *pcar, int distance){
+    cout << "1. Tankowanie do pelna:\n";
+    pcar->Refuel(1000);
+    cout << "2. Jazda testowa: " << distance << " km:\n";
+    pcar->Start();
+    pcar->Drive(distance);
+    pcar->Stop();
+
+    cout << "### Koniec testu serwisowego ###\n";
+    cout << "#############################################################\n\n";
+}
