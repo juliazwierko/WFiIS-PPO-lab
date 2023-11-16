1aaee78bb5028ce66966735a13bba304588c563a
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-16 15:04:49 +0100

    Update Polygon.cpp

--- 05/src/Polygon.cpp
+++ 05/src/Polygon.cpp
@@ -1,24 +1,20 @@
 #include "Polygon.h"
 #include "Point.h"
+#include <cstring>
 
-int Polygon::m = 0;
-
-void Polygon::Presentation(){\
-    cout << "\n### Prezentacja wielokata ###" << endl;
-    if(m == 1){
-        cout << "Nazwa:\n";
-    } 
-    if(m == 2){
-        cout << "Nazwa: Zerokat\n";
+void Polygon::Presentation(){
+    cout << "\n### Prezentacja wielokata ###\n";
+    cout << "Nazwa: " << _Name2 << endl;
+    cout << "Liczba wierzcholkow: " << _ilosc_wierzcholkow << endl ;
+    if(_ilosc_wierzcholkow == 3){
+        // cout << "Wierzcholek - nazwa: " << Point::GetName(Point a); 
     }
-    if(m == 3){
-        cout << "Nazwa: Nastepny zerokat\n";
-    }
-    cout << "Liczba wierzcholkow: " << _liczbawierzcholkow << endl;
-    cout << "Wierzcholek - nazwa: A, " << "X = " << _x << ", Y = " << _y << endl;
 }
 
-void Polygon::SetName(const char* Nazwa){
-    // _name = Name;
-    m++;
+void Polygon::SetName(const char* Name){
+    _Name2 = new char[strlen(Name)+1];
+    strcpy(const_cast<char*>(_Name2), Name); 
 }
+// ### Prezentacja wielokata ###
+// Nazwa:
+// Liczba wierzcholkow: 0
