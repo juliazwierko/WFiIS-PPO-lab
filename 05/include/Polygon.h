560266cd82108632f55dc5f87d8f9f0966d81115
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-16 15:05:05 +0100

    Update Polygon.h

--- 05/include/Polygon.h
+++ 05/include/Polygon.h
@@ -1,96 +1,65 @@
-#pragma once
+#pragma once 
 #include <iostream>
-#include <cmath>
 #include "Point.h"
 using namespace std;
 
 class Polygon{
 public:
-    /**
-     * konstruktor bezparametrowy
-    */
     Polygon(){
-        m++;
-        cout <<"\nWywolanie konstruktora Polygon...\n\n";
-        _x = 0;
-        _y = 0;
-        _z = 0;
-        _liczbawierzcholkow = 0;
+        cout << "Wywolanie konstruktora Polygon...\n";
+        const char* temp = "";
+        _Name2 = new char[strlen(temp)+1];
+        strcpy(const_cast<char*>(_Name2), temp); 
+        this->_ilosc_wierzcholkow = 0;
     }
-    /**
-     * konstruktor z parametrem Name
-    */
-    Polygon(const char *Nazwa){
-        cout << "Wywolanie konstruktora Polygon...\n\n";
-        _liczbawierzcholkow = 0;
-        m++;
+    Polygon(const char* Name){
+        cout << "\nWywolanie konstruktora Polygon...\n";
+        _Name2 = new char[strlen(Name)+1];
+        strcpy(const_cast<char*>(_Name2), Name); 
+        this->_ilosc_wierzcholkow = 0;
     }
-    /**
-     * konstruktor z parametrami name, oraz elementami klasy Point- punktu a, b i c
-    */
-    Polygon(const char *Nazwa, ::Point a, ::Point b, ::Point c){
-        m++;
-        cout <<"\nWywolanie konstruktora Polygon...\n";
-        cout <<"\nWywolanie konstruktora Point...\n";
-        cout <<"\nWywolanie konstruktora Point...\n";
-        cout <<"\nWywolanie konstruktora Point...\n";
+    Polygon(const char* Name, Point a, Point b, Point c){
+        cout << "\nWywolanie konstruktora Polygon...\n\n";
+        _Name2 = new char[strlen(Name)+1];
+        strcpy(const_cast<char*>(_Name2), Name); 
+        //
+        Point new_a;
+        Point new_b;
+        Point new_c;  
+        new_a = a; new_b =b; new_c = c;      
+        //
+        this->_ilosc_wierzcholkow = 3;
     }
-    Polygon(const char *Nazwa, ::Point a, ::Point b, ::Point c, ::Point d){
-
+    Polygon(const char* Name, Point a, Point b, Point c, Point d){
+        cout << "\nWywolanie konstruktora Polygon...\n\n";
+        _Name2 = new char[strlen(Name)+1];
+        strcpy(const_cast<char*>(_Name2), Name); 
+        //
+        Point new_aa;
+        Point new_bb;
+        Point new_cc;  
+        Point new_dd;
+        new_aa = a; new_bb =b; new_cc = c; new_dd = d;      
+        //
+        this->_ilosc_wierzcholkow = 4;
     }
-    Polygon(const char *Nazwa, int liczba, ::Point* tab){
-
+    Polygon(const char* Name, double number, Point tab[]){
+        _Name2 = new char[strlen(Name)+1];
+        strcpy(const_cast<char*>(_Name2), Name); 
+        //
+        _ilosc_wierzcholkow = number;
+        //
     }
-    // Polygon(const char *Nazwa, int liczba, ::Point tab){
-
-    // }
-   
-
-    /**
-     * @brief funkcja reprezentacji wspolrzednych 
-     * 
-     * funkcja wypisuje wszystkie dane funkcji 
-     * 
-     * @param[in] nic nie przyjmuje 
-     * @return void nic nie zwraca 
-     * 
-    */
     void Presentation();
-    /**
-     * @brief funkcja ustawia imie
-     * 
-     * funkcja istawia imie
-     * 
-     * @param[in] name - to wskaznik typu const chat - nasze imie
-     * @return void nic nie zwraca 
-     * 
-    */
-    void SetName(const char* Nazwa);
-    Point* GetVertices(){
-        Point *tab;
-        return tab;
-    }
-    /**
-     * @brief Funkcja ustawienia imiona. ilosci wierzcholkow oraz parametrow
-     * 
-     * funkcja przepisuje nowemu obiektowi imie, ilosc wierzcholkow oraz przypisuje warosci
-     * 
-     * @param[in] liczba - ilosc wierzcholkow. name - nazwa, a i b - parametry
-     * @return nic nie zwraca 
-    */
-    void SetVertice(int liczba, const char *name, double a, double b){
-        _liczbawierzcholkow = liczba;
-        _x = a;
-        _y = b;
-    }
-    void SetNewVertices(double a, ::Point *tab){
-
-    }
+    void SetName(const char* Name);
+    
 private:
-    const char* Name;
-    double _x;
-    double _y;
-    double _z;
-    double _liczbawierzcholkow;
-    static int m;
+    const char* _Name2;
+    int _ilosc_wierzcholkow;
 };
+
+// Wywolanie konstruktora Polygon...
+
+// ### Prezentacja wielokata ###
+// Nazwa: Nastepny zerokat
+// Liczba wierzcholkow: 0
