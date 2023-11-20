757989f12d81ce855cdc3e077dffc935f0d9a937
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-20 22:59:44 +0100

    Update Polygon.cpp

--- 05/src/Polygon.cpp
+++ 05/src/Polygon.cpp
@@ -6,15 +6,26 @@ void Polygon::Presentation(){
     cout << "\n### Prezentacja wielokata ###\n";
     cout << "Nazwa: " << _Name2 << endl;
     cout << "Liczba wierzcholkow: " << _ilosc_wierzcholkow << endl ;
-    if(_ilosc_wierzcholkow == 3){
-        // cout << "Wierzcholek - nazwa: " << Point::GetName(Point a); 
+    if(_ilosc_wierzcholkow>0){
+        for(size_t i = 0; i < _ilosc_wierzcholkow; i++){
+            cout << "Wierzcholek - nazwa: " << punkty[i].GetName() << " X = " << punkty[i].GetX() << " ,Y = " << punkty[i].GetY()  <<  endl;
+        }
     }
 }
 
-void Polygon::SetName(const char* Name){
-    _Name2 = new char[strlen(Name)+1];
-    strcpy(const_cast<char*>(_Name2), Name); 
+void Polygon::SetName(string Name){
+    _Name2 = Name;
 }
-// ### Prezentacja wielokata ###
-// Nazwa:
-// Liczba wierzcholkow: 0
+
+Point * Polygon::GetVertices(){
+    return punkty;
+}
+
+void Polygon::SetVertice(int number, string name, double x, double y){
+    if(number-1 < 0 or number-1 > _ilosc_wierzcholkow-1){
+        return;
+    }
+    punkty[number-1].SetName(name);
+    punkty[number-1].SetX(x);
+    punkty[number-1].SetY(y);
+}
