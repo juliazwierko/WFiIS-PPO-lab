2d05e156e6f2ea9f7bd3261dd9f479e89725156d
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-20 23:00:04 +0100

    Update Point.h

--- 05/include/Point.h
+++ 05/include/Point.h
@@ -1,30 +1,25 @@
 #pragma once
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Point{
 public:
     Point(){
         cout << "Wywolanie konstruktora Point...\n\n";
-        const char* temp = "";
-        _Name = new char[strlen(temp)+1];
-        strcpy(const_cast<char*>(_Name), temp); 
-        _x = 0;
-        _y = 0;
     }
-    Point(const char* Name){
+    Point(string Name){
         cout << "Wywolanie konstruktora Point...\n\n";
-        _Name = new char[strlen(Name)+1];
-        strcpy(const_cast<char*>(_Name), Name); 
-        _x = 0;
-        _y = 0;
+        SetName(Name);
     }   
-    Point(const char* Name, double x, double y){
+    Point(string Name, double x, double y){
         cout << "Wywolanie konstruktora Point...\n\n";
-        _Name = new char[strlen(Name)+1];
-        strcpy(const_cast<char*>(_Name), Name); 
-        _x = x;
-        _y = y;
+        SetName(Name);
+        SetX(x);
+        SetY(y);
+    }
+    ~Point(){
+        cout << "Wywolanie destruktora Point...\n\n";
     }
     double GetX(){
         return _x;
@@ -32,16 +27,16 @@ public:
     double GetY(){
         return _y;
     }
-    const char* GetName(Point a) const {
-        return a._Name;
+    string GetName() const {
+        return _Name;
     }
     void Presentation();
-    void SetName(const char* Name);
+    void SetName(string Name);
+    
     void SetX(double x);
     void SetY(double y);
-    const char* _Name;
 private:
-    double _x;
-    double _y;
-    // const char* _Name;
+    double _x = 0;
+    double _y = 0;
+    string _Name;
 };
