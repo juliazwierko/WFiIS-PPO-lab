ebbd10406a644307e9ae2ef250582d42441c5bf2
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-20 23:00:39 +0100

    Update Polygon.h

--- 05/include/Polygon.h
+++ 05/include/Polygon.h
@@ -1,5 +1,6 @@
 #pragma once 
 #include <iostream>
+#include <string>
 #include "Point.h"
 using namespace std;
 
@@ -7,59 +8,59 @@ class Polygon{
 public:
     Polygon(){
         cout << "Wywolanie konstruktora Polygon...\n";
-        const char* temp = "";
-        _Name2 = new char[strlen(temp)+1];
-        strcpy(const_cast<char*>(_Name2), temp); 
-        this->_ilosc_wierzcholkow = 0;
     }
-    Polygon(const char* Name){
+    Polygon(string Name){
         cout << "\nWywolanie konstruktora Polygon...\n";
-        _Name2 = new char[strlen(Name)+1];
-        strcpy(const_cast<char*>(_Name2), Name); 
-        this->_ilosc_wierzcholkow = 0;
+        SetName(Name);
+        
     }
-    Polygon(const char* Name, Point a, Point b, Point c){
+    Polygon(string Name, Point a, Point b, Point c){
         cout << "\nWywolanie konstruktora Polygon...\n\n";
-        _Name2 = new char[strlen(Name)+1];
-        strcpy(const_cast<char*>(_Name2), Name); 
-        //
-        Point new_a;
-        Point new_b;
-        Point new_c;  
-        new_a = a; new_b =b; new_c = c;      
-        //
-        this->_ilosc_wierzcholkow = 3;
+        _ilosc_wierzcholkow = 3;
+        punkty = new Point [3];
+
+        punkty[0] = a;
+        punkty[1] = b;
+        punkty[2] = c;
     }
-    Polygon(const char* Name, Point a, Point b, Point c, Point d){
+    Polygon(string Name, Point a, Point b, Point c, Point d){
         cout << "\nWywolanie konstruktora Polygon...\n\n";
-        _Name2 = new char[strlen(Name)+1];
-        strcpy(const_cast<char*>(_Name2), Name); 
-        //
-        Point new_aa;
-        Point new_bb;
-        Point new_cc;  
-        Point new_dd;
-        new_aa = a; new_bb =b; new_cc = c; new_dd = d;      
-        //
-        this->_ilosc_wierzcholkow = 4;
+        _ilosc_wierzcholkow = 4;
+        punkty = new Point [4];
+        
+        punkty[0] = a;
+        punkty[1] = b;
+        punkty[2] = c;
+        punkty[3] = d; 
     }
-    Polygon(const char* Name, double number, Point tab[]){
-        _Name2 = new char[strlen(Name)+1];
-        strcpy(const_cast<char*>(_Name2), Name); 
-        //
+    Polygon(string Name, double number, Point tab[]){
+        cout << "\nWywolanie konstruktora Polygon...\n\n";
+        _Name2 = Name;
         _ilosc_wierzcholkow = number;
-        //
+        
+        punkty = new Point [_ilosc_wierzcholkow];
+        for(int i = 0; i < _ilosc_wierzcholkow; i++){
+            punkty[i] = tab[i];
+        }
     }
+    void SetNewVertices(int number, Point *a){
+        _ilosc_wierzcholkow = number;
+        delete [] punkty;
+        punkty = new Point [_ilosc_wierzcholkow];
+        for(int i = 0; i < _ilosc_wierzcholkow; i++){
+            punkty[i] = *(a+i);
+        }
+    }
+    ~Polygon(){
+        cout << "\nWywolanie destruktora Polygon...\n";
+        delete [] punkty;
+    }
+    void SetVertice(int number, string name, double x, double y);
     void Presentation();
-    void SetName(const char* Name);
-    
+    void SetName(string Name);
+    Point * GetVertices();
 private:
-    const char* _Name2;
-    int _ilosc_wierzcholkow;
+    string _Name2;
+    int _ilosc_wierzcholkow = 0;
+    Point *punkty = nullptr;
 };
-
-// Wywolanie konstruktora Polygon...
-
-// ### Prezentacja wielokata ###
-// Nazwa: Nastepny zerokat
-// Liczba wierzcholkow: 0
