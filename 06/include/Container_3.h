4c5b90cfe065edc82fcb5e3b4ef9975a144c6a3e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-11-23 11:55:39 +0100

    Update Container_3.h

--- 06/include/Container_3.h
+++ 06/include/Container_3.h
@@ -3,71 +3,14 @@
 
 class Container_3 {
 public:
-    /**
-     * to jest konstruktor, przypisujemy mu size 0
-    */
     Container_3(int initialSize);
-    /**
-     * to jest destruktor
-    */
     ~Container_3();
-
-
-    /**
-     * to jest funkcja dodawania wartosciej fo tablicy
-     * 
-     * @brief funkcja dodaje to tablicy elementy
-     * 
-     * @param[in] value - wartosci do dodawania
-     * @return - nic nie zwraca
-    */
     void Add(int value);
-     /**
-     * to jest funkcja usuwania wartosciej z tablicy
-     * 
-     * @brief funkcja usuwa z tablicy elementy
-     * 
-     * @param[in] nic nie przyjmuje
-     * @return - nic nie zwraca
-    */
     int Delete();
-     /**
-     * to jest funkcja wyczysczenia wartosciej z tablicy
-     * 
-     * @brief funkcja usuwa z tablicy elementy
-     * 
-     * @param[in] nic nie przyjmuje
-     * @return - nic nie zwraca
-    */
     void Clear();
-     /**
-     * to jest funkcja wypisania wartosciej z tablicy
-     * 
-     * @brief funkcja wypisuje z tablicy elementy
-     * 
-     * @param[in] nic nie przyjmuje
-     * @return - nic nie zwraca
-    */
     void Print() const;
-     /**
-     * to jest funkcja sprawdzenia wypelnionosc tablicy
-     * 
-     * @brief funkcja sprawdza z tablicy elementy
-     * 
-     * @param[in] nic nie przyjmuje
-     * @return - nic nie zwraca
-    */
     bool IsEmpty() const;
-       /**
-     * to jest funkcja sprawdzenia wypelnionosc tablicy
-     * 
-     * @brief funkcja sprawdza z tablicy elementy
-     * 
-     * @param[in] nic nie przyjmuje
-     * @return - nic nie zwraca
-    */
     bool IsFull() const;
-    //rozmiar zadanej teblicy;
     int Size() const;
 
 private:
