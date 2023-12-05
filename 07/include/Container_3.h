d9abef3d1d582ef490ab51e02868f66d28ccc4c1
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-12-05 17:32:41 +0100

    Update Container_3.h

--- 07/include/Container_3.h
+++ 07/include/Container_3.h
@@ -1,87 +1,27 @@
 #pragma once
-#include <iostream>
+#include <iostream> 
 
-class Container_3 {
+class Container_3{
 public:
-/** @brief Konstruktor
-*
-* Funkcja inicjalizuje klase domyslnymi wartosciami
-*
-* @return nic nie zwraca
-*/ 
-    Container_3(int initialSize);
-
-/** @brief Destruktor
-*
-* Funkcja niszczy obiekt
-*
-* @return nic nie zwraca
-*/ 
-    ~Container_3();
-
-/** @brief Usuwanie elementu
-*
-* Funkcja usuwa element
-*
-* @return int
-*/
+    Container_3(int initial_size){
+        capacity = initial_size;
+        size = 0;
+        tab = new int[capacity];
+    }
+    ~Container_3(){
+        delete [] tab;
+        tab = nullptr;
+    }
+    void Add(int value);
     int Delete();
-
-/** @brief Czy pusta.
-*
-* Funkcja zwraca informacje czy jest pusta
-*
-* @return bool, nic nie zwraca
-*/
     bool IsEmpty() const;
-
-/** @brief Czy pelna
-*
-* Funkcja zwraca informacje czy jest pelna.
-*
-*
-* @return void, nic nie zwraca
-*/
     bool IsFull() const;
-
-/** @brief Dodawanie elementu.
-*
-* Funkcja dodaje element.
-*
-* @param[in] i  wartosc dodawanego elementu 
-* @return void, nic nie zwraca
-*/
-    void Add(int i);
-
-/** @brief Wyswietlanie zawartosci
-*
-* Funkcja wyswietla zawartosc klasy
-*
-* @return void, nic nie zwraca
-*/
     void Print() const;
-
-/** @brief Oczyszczanie
-*
-* Funkcja resetuje zawartosc pola size
-*
-* @return void, nic nie zwraca
-*/
+    int Size() const;
     void Clear();
 
-/** @brief Funkcja zwracajaca rozmiar.
-*
-* Funkcja zwraca prywatne pole klasy
-*
-* @return void, nic nie zwraca
-*/
-    int Size() const;
-
-
 private:
-    int* arr; 
-    int capacity; 
+    int capacity;
     int size;
-
-    void Resize();
+    int* tab;
 };
