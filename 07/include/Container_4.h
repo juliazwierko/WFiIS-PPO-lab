9d5e7ec0538b000d5dbaec8d6dceaf5d582a0ee4
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-12-05 17:32:50 +0100

    Update Container_4.h

--- 07/include/Container_4.h
+++ 07/include/Container_4.h
@@ -2,66 +2,16 @@
 #include <iostream>
 #include <vector>
 
-class Container_4 {
+class Container_4{
 public:
-/** @brief Usuwanie elementu z wektora
-*
-* Funkcja ktora usuwa element element wektora
-*
-* @return int
-*/
-    int Delete(); 
-
-/** @brief Sprawdzenia czy wektor jest pusty
-*
-* Funkcja zwraca informacje czy jest wektor pusty
-*
-* @return bool, nic nie zwraca, wartosc logiczna
-*/
-    bool IsEmpty() const; 
-
-/** @brief Czywektor jest pelny
-*
-* Funkcja zwraca informacje czy jest pelna.
-*
-*
-* @return void, nic nie zwraca
-*/
-    bool IsFull() const; 
-
-/** @brief Dodawanie elementu.
-*
-* Funkcja dodaje element.
-*
-* @param[in] i  wartosc dodawanego elementu 
-* @return void, nic nie zwraca
-*/
-    void Add(int i); 
-
-/** @brief Wyswietlanie zawartosci wektora
-*
-* Funkcja wyswietla zawartosc klasy
-*
-* @return void, nic nie zwraca
-*/
-    void Print() const; 
-
-/** @brief Oczyszczanie
-*
-* Funkcja resetuje zawartosc pola size
-*
-* @return void, nic nie zwraca
-*/
-    void Clear(); 
-
-/** @brief Funkcja zwraca rozmiar.
-*
-* Funkcja zwraca prywatne pole klasy
-*
-* @return void, nic nie zwraca
-*/
-    int Size() const; 
-
+    int Delete();
+    bool IsEmpty() const;
+    bool IsFull() const;
+    void Add(int value);
+    void Print() const;
+    int Size() const;
+    void Clear();
 private:
-    std::vector<int> vec;
+    int capacity = 4;
+    std::vector <int> tab;
 };
