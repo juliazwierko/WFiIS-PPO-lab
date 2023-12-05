0791d45c4c326cd96aece886aef1746351c93209
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-12-05 17:31:00 +0100

    Update Container_3.cpp

--- 06/src/Container_3.cpp
+++ 06/src/Container_3.cpp
@@ -1,52 +1,13 @@
 #include "Container_3.h"
-#include <iostream>
 
-Container_3::Container_3(int initialSize){
-    size = 0; 
-    capacity = initialSize;
-    data = new int[capacity];
-}
-Container_3::~Container_3() {
-    delete[] data;
-}
-
-void Container_3::Add(int value) {
-    if (size < capacity) {
-        data[size++] = value;
-    } else {
-        //zamiana rozmiaru w przypadku przepelnienia tablicy!!!
-        int* newData = new int[2 * capacity];
-        for (int i = 0; i < size; ++i) {
-            newData[i] = data[i];
-        }
-        delete[] data;
-        data = newData;
-        capacity *= 2;
-        data[size++] = value;
-        // delete [] newData;
+int Container_3::Delete(){
+    if(!IsEmpty()){
+        return tab[--size];
     }
-}
-
-int Container_3::Delete() {
-    if (!IsEmpty()) {
-        return data[--size];
-    } else {
-        std::cerr << "#BLAD: Obiekt pusty" << std::endl;
-        return -1; 
+    else{
+        std::cout << "#BLAD: Obiekt pusty";
+        return -1;
     }
-}
-
-void Container_3::Clear() {
-    size = 0;
-}
-
-void Container_3::Print() const {
-    std::cout << "#Zawartosc obiektu: ";
-    std::cout << std::endl;
-    for (int i = 0; i < size; ++i) {
-        std::cout << data[i] << ",";
-    }
-    std::cout << std::endl;
 }
 
 bool Container_3::IsEmpty() const {
@@ -57,6 +18,33 @@ bool Container_3::IsFull() const {
     return size == capacity;
 }
 
-int Container_3::Size() const {
+void Container_3::Add(int value){
+    if(!IsFull()){
+        tab[size++] = value;
+    }
+    else{
+        int *new_tab = new int[2*capacity];
+        for(int i = 0; i < size; i++){
+            new_tab[i] = tab[i];
+        }
+        delete [] tab;
+        tab = new_tab;
+        tab[size++] = value;
+    }
+}
+
+void Container_3::Print() const{
+    std::cout << "#Zawartosc obiektu:\n";
+    for(int i = 0; i < size; i++){
+        std::cout << tab[i] << ",";
+    }
+    std::cout << std::endl;
+}
+
+int Container_3::Size() const{
     return size;
+}
+
+void Container_3::Clear(){
+    size = 0;
 }
