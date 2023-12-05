fa564f45a73d35f8783a5e6975e1e4d4355c0b29
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-12-05 17:31:09 +0100

    Update Container_4.cpp

--- 06/src/Container_4.cpp
+++ 06/src/Container_4.cpp
@@ -1,47 +1,41 @@
 #include "Container_4.h"
-#include <iostream>
 
-Container_4::Container_4() {}
-
-Container_4::~Container_4() {}
-
-void Container_4::Add(int value) {
-    data.push_back(value);
-}
-
-int Container_4::Delete() {
-    if (!IsEmpty()) {
-        int value = data.back();
-        data.pop_back();
-        return value;
-    } else {
-        std::cerr << "#BLAD: Obiekt pusty" << std::endl;
-        return -1; 
+int Container_4::Delete(){
+    if(!IsEmpty()){
+        int data = tab.back();
+        tab.pop_back();      
+        return data;
+    }
+    else{
+        std::cout << "#BLAD: Obiekt pusty\n";
+        return -1;
     }
 }
 
-void Container_4::Clear() {
-    data.clear();
+bool Container_4::IsEmpty() const{
+    return tab.empty(); 
 }
 
-void Container_4::Print() const {
-    std::cout << "#Zawartosc obiektu: ";
-    // for (int i = value; i < data; i++){ 
-    // komientarz dla siebie*
-    for (int value : data) {
+bool Container_4::IsFull() const{
+    return false;
+}
+
+void Container_4::Add(int value){
+    tab.push_back(value);
+}
+
+void Container_4::Print() const{
+    std::cout << "#Zawartosc obiektu:\n";
+    for(int value : tab){
         std::cout << value << ",";
     }
     std::cout << std::endl;
 }
 
-bool Container_4::IsEmpty() const {
-    return data.empty();
+int Container_4::Size() const{
+    return tab.size();
 }
 
-bool Container_4::IsFull() const {
-    return false;
-}
-
-int Container_4::Size() const {
-    return data.size();
+void Container_4::Clear(){
+    tab.clear();
 }
