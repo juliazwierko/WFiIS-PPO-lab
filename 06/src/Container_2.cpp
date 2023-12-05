0a41d8b8e49c630e6f7b54bc443d8d14fc3a1a5a
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-12-05 17:30:50 +0100

    Update Container_2.cpp

--- 06/src/Container_2.cpp
+++ 06/src/Container_2.cpp
@@ -1,51 +1,44 @@
 #include "Container_2.h"
-#include <iostream>
 
-Container_2::Container_2(){
-    size = 0;
+int Container_2::Delete(){
+    if(!IsEmpty()){
+        return tab[size--];
+    }
+    else{
+        std::cout << "#BLAD: Obiekt pusty\n";
+        return -1;
+    }
+}
+    
+bool Container_2::IsEmpty() const{
+    return size == 0;
 }
 
-Container_2::~Container_2() {
-
+bool Container_2::IsFull() const{
+    return size == 4;
 }
 
-void Container_2::Add(int value) {
-    if (size < capacity) {
-        data[size++] = value;
-    } else {
-        std::cerr << "#BLAD: Obiekt zapelniony" << std::endl;
+void Container_2::Add(int number){
+    if(!IsFull()){
+        tab[size++] = number;
+    }
+    else{
+        std::cout << "#BLAD: Obiekt zapelniony\n";
     }
 }
 
-int Container_2::Delete() {
-    if (!IsEmpty()) {
-        return data[--size];
-    } else {
-        std::cerr << "#BLAD: Obiekt pusty" << std::endl;
-        return -1; 
-    }
-}
-
-void Container_2::Clear() {
-    size = 0;
-}
-
-void Container_2::Print() const {
-    std::cout << "#Zawartosc obiektu: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << data[i] << ",";
+void Container_2::Print() const{
+    std::cout << "#Zawartosc obiektu:\n";
+    for(int i = 0; i < size; i++){
+        std::cout << tab[i] << ",";
     }
     std::cout << std::endl;
 }
 
-bool Container_2::IsEmpty() const {
-    return size == 0;
-}
-
-bool Container_2::IsFull() const {
-    return size == capacity;
-}
-
-int Container_2::Size() const {
+int Container_2::Size() const{
     return size;
+}
+
+void Container_2::Clear(){
+    size = 0;
 }
