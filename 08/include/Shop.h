16a090b03f754cb4f2c18e1bda2379d63975ca30
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-12-11 19:48:44 +0100

    Update Shop.h

--- 08/include/Shop.h
+++ 08/include/Shop.h
@@ -1,38 +1,56 @@
-#pragma once
-#include <iostream>
+#pragma once 
 #include <vector>
+#include <iostream>
+
+using namespace std;
 
 class Shop;
 
 class Product{
     friend class Shop;
 public:
-    Product (int a, int b);
+    Product(int typ, int ilosc) : _typ(typ), _ilosc(ilosc) {}    
     void Print() const;
-    void SetCard(int a);
-    
+    void SetCard(int num);
 private:
-    int _n1;
-    int _n2;
+    int _typ;
+    int _ilosc;
 };
 
 class Shop{
 public:
-    Shop();
-    Shop(Product elem1);
-    Shop(Product elem1, Product elem2);
-    Shop(int size, Product *t[3]);
-    Shop(std::vector<Product> v);
-    Shop(Shop&);
-    ~Shop();
-    void Print()const;
-    void Remove();
-    void Add(Product elem);
-    void SetCard (int i);
-    Shop(Shop&& other);
-    Shop& operator=(Shop&& other);
-    void Clear();
+    Shop(){
+
+    }
+    Shop(Product product){
+        shop.push_back(product);
+    }
+    Shop(Product product1, Product product2){
+        shop.push_back(product1);
+        shop.push_back(product2);
+    }
+    Shop(int size, Product* t[3]){
+        for(int i = 0; i < size; i++){
+        shop.push_back(*t[i]);
+        }   
+    }
+    //konstruktor kopiujacy
+    Shop(const vector <Product>& dane){
+        for(Product range : dane){
+            shop.push_back(range);
+        }
+    }
+    //konstruktor kopiujacy
+    Shop(const Shop &copy_shop){
+        for(int i = 0; i < copy_shop.shop.size(); i++){
+            shop.push_back(copy_shop.shop[i]);
+        }
+    }
     
+    void Clear();
+    void Add(Product item);
+    void Print() const;
+    void Remove();
 private:
-std::vector <Product> vector;
+    vector <Product> shop;
 };
