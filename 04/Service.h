1072d1dc9728feda2bdabdc6c760c88a6efed0f8
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-12-27 11:35:14 +0100

    Update Service.h

--- 04/Service.h
+++ 04/Service.h
@@ -2,33 +2,8 @@
 
 class Service {
 public:
-    /** @brief Sprawdzenie kopi pojazdu
-    *
-    * Test serwisowy dla skopiowanego pojazdu
-    *
-    * @param[in] car pojazd
-    * @param[in] distance dystans do przejechania
-    * @return funkcja nic nie zwraca
-    */
     void TestCopy(Car car, int distance);
-
-    /** @brief Sprawdzenie pojazdu
-    *
-    * Test serwisowy dla pojazdu
-    *
-    * @param[in] pcar wskaznik do pojazdu
-    * @param[in] distance dystans do przejechania
-    * @return funkcja nic nie zwraca
-    */
     void Test(Car * pcar, int distance);
 };
 
-/** @brief Sprawdzenie pojazdu
-*
-* Test serwisowy dla pojazdu
-*
-* @param[in] pcar wskaznik do pojazdu
-* @param[in] distance dystans do przejechania
-* @return funkcja nic nie zwraca
-*/
 void TestCar(Car *pcar, int distance);
