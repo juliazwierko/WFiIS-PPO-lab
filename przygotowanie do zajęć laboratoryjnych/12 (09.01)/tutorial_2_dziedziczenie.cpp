067674466e2d54072a1df6ec5a19b11742de2d78
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2024-01-09 20:16:33 +0100

    Add files via upload

---
+++ przygotowanie do zajęć laboratoryjnych/12 (09.01)/tutorial_2_dziedziczenie.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person{
+public:
+    Person(string name, unsigned age){
+        this->name = name;
+        this->age = age;
+    }
+    void print() const
+    {
+        std::cout << "Name: " << name << "\tAge: " << age << std::endl;
+    }
+
+private:
+    string name;
+    unsigned age;
+};
+
+class Employee: public Person{
+public:
+    Employee(string name, int age, string company) : Person(name, age){
+        this->company = company;
+    }
+private:
+    string company;
+};
+
+int main(){
+    Person person {"Julia", 18};
+    person.print(); // Name: Tom Age: 38
+
+    Employee employee {"Pawel", 23, "Nokia"};
+    employee.print();
+    return 0;
+}
