7dc8c302fb61bbeca06a59aa09c18970f9e91dd2
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2024-03-03 11:06:13 +0100

    Update Array.h

--- 03/Array.h
+++ 03/Array.h
@@ -1,7 +1,37 @@
-#pragma once
-#include <iostream>
 using namespace std;
 
-int* InitArray(int size);
-void CreateSequence(int* tab, int size);
-void DeleteArray(int* tab);
+int* InitArray(int size){
+    int* tab = new int[size];
+    return tab;
+}
+
+void CreateSequence(int* tab, int size){
+    if(size<0){
+        cout << "Tablica jest pusta, albo podan zly size" << endl;
+    }
+    if(size == 1){
+        tab[0] = 1;
+    }
+    else{
+        tab[0] = 1;
+        tab[1] = 1;
+        int temp = 2;
+        for(int i=2; i<size; i++){
+            tab[i] = temp * tab[i-1];
+            temp++;
+            // cout << tab[i] << endl;
+        }
+    }
+}
+
+void DeleteArray(int* tab){
+    delete [] tab;
+}
+int* ChangeArraySize(int *array, int prevSize, int newSize){
+    int* newArray = new int[newSize];
+    for (int i = 0; i < prevSize; i++) {
+        newArray[i] = array[i];
+    }
+    delete [] array;
+    return newArray;
+}
